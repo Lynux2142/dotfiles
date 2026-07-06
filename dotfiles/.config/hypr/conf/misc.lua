762be9f04d4c2706459cc59c23b2997779a862aa
Lucas Guillerot lguiller <lguiller@student.42.fr>
2026-07-06 13:17:25 +0200

    fix typo

--- dotfiles/.config/hypr/conf/misc.lua
+++ dotfiles/.config/hypr/conf/misc.lua
@@ -5,7 +5,7 @@ hl.config({
         disable_splash_rendering = true,
         initial_workspace_tracking = 1,
         on_focus_under_fullscreen = 1,
-        allow_session_lock_restore = true
+        allow_session_lock_restore = true,
         focus_on_activate = true,
     },
 })
